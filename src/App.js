25b41997a8383885676ecd8f8aa79ad8f253e3bb
Imran shaikh <imranpathan7040@gmail.com>
2023-01-10 21:27:55 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import CheckOut from "./Components/CheckOut";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+function App() {
+  return (
+    <>
+      <Router>
+
+        <Routes>
+
+          <Route path="/" element={<div><Header /><Home /></div>} />
+          <Route path="/checkout" element={<div><Header /> <CheckOut /></div>} />
+        </Routes>
+
+
+
+
+      </Router>
+
+    </>
+
+  );
+}
+
+export default App;
