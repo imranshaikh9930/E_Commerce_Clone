3e0d9b3a0fef27766c7b5070903d9872189adb58
Imran shaikh <imranpathan7040@gmail.com>
2023-01-14 15:37:25 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import Header from "./Components/Header";
 import Home from "./Components/Home";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./Components/"
 function App() {
   return (
     <>
       <Router>
+      
 
         <Routes>
-
-          <Route path="/" element={<div><Header /><Home /></div>} />
-          <Route path="/checkout" element={<div><Header /> <CheckOut /></div>} />
+          <Route path="/login" element={<Login />} />
+         
+          <Route path="/" element={<div> <Header /> <Home /></div>} />
+          <Route path="/checkout" element={<div>  <Header /> <CheckOut /></div>} />
         </Routes>
 
 
